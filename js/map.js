590689869798d492efbd92b7b8b0e1de940d3e1a
Evan Virevialle <83653214+evirevialle@users.noreply.github.com>
2024-03-24 16:56:27 +0100

    Update map.js

--- js/map.js
+++ js/map.js
@@ -17,7 +17,7 @@ function initializeMap () {
 
 
 
-    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
         maxZoom: 20,
         minZoom: 2,
